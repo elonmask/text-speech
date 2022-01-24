2a166086defb9afba130a611b4af13a956d8b6d3
Sergei Voronov <woronowserg@gmail.com>
2022-01-24 22:42:20 +0200

    new brain

--- brain/main.py
+++ brain/main.py
@@ -1,27 +1,35 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.trainers import ListTrainer
+
 from flask import Flask
 from flask_cors import CORS,cross_origin
+from flask import request
 app = Flask(__name__)
 CORS(app)
 
+from convokit import Corpus, download
+corpus_movie = Corpus(filename=download("movie-corpus"))
+corpus_conv_reddit = Corpus(filename=download("conversations-gone-awry-cmv-corpus"))
+corpus_friends = Corpus(filename=download("friends-corpus"))
 
-chatbot = ChatBot('Rona Obvious')
+chatbot = ChatBot('Tyler')
 
 # Create a new trainer for the chatbot
 trainer = ChatterBotCorpusTrainer(chatbot)
+trainer.train("chatterbot.corpus.english")
+trainer.train(corpus_movie)
+trainer.train(corpus_conv_reddit)
+trainer.train(corpus_friends)
 
-# Train the chatbot based on the english corpus
-#trainer.train("chatterbot.corpus.english")
 
 @app.route("/", methods=['GET'])
-@cross_origin(origin='http://localhost:3000')
 def index():
-   ##response = chatbot.get_response("How are you?")
-   ##print(response)
+   speech = request.args.get('speech')
+   response = chatbot.get_response(speech)
    return {
-   "response": "Response"
+   "response": str(response)
    }
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5000, debug=False)
